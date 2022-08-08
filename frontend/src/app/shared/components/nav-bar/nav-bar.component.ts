e389ae66f7c25554fafd4634d51bfe55e137b12a
Hilbert <hil.beer.t.git@gmail.com>
2022-08-07 21:26:03 -0300

    add logo

--- frontend/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ frontend/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  redirect(route: string){
+    if (route === 'home'){
+      this.router.navigate(['/home'])
+    } else {
+      this.router.navigate(['/arvores'])
+    }
   }
 
 }
